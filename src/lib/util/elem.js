d4a2761f28f1ebfa1acb0332604751e1607ddb2d
nakajmg <nakajima.jmg@gmail.com>
2015-04-20 19:13:40 +0900

    add util method

--- src/lib/util/elem.js
+++ src/lib/util/elem.js
@@ -1,15 +1,36 @@
-export default function elem({className, type, text}) {
+export default function elem({className, type, text, attribute}) {
   var el;
   type = type || "div";
   el = document.createElement(type);
   
   if (className) {
-    el.classList.add(className);
+    addClass(el, className);
   }
   
   if (text) {
     el.textContent = text;
   }
   
+  if (attribute) {
+    attr(el, attribute.name, attribute.value);
+  }
+  
   return el;
+}
+
+export function addClass(el, className) {
+  el.classList.add(className);
+}
+
+export function removeClass(el, className) {
+  el.classList.remove(className);
+}
+
+export function attr(el, attributeName, newValue) {
+  if (newValue) {
+    el.setAttribute(attributeName, newValue);
+  }
+  else {
+    return el.getAttribute(attributeName);
+  }
 }
